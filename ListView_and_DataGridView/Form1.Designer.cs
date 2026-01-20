b55e90cc17a2e54a2442c435a05b58d7d8725e2e
アンネ <krisjulom@gmail.com>
2026-01-20 14:07:30 +0800

    Add files via upload

---
+++ ListView_and_DataGridView/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace ListView_and_DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lvRecords = new System.Windows.Forms.ListView();
+            this.StudentName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StudentID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Course = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DataGV1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lbl_ID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblCourse = new System.Windows.Forms.Label();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.picHeader = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGV1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHeader)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lvRecords
+            // 
+            this.lvRecords.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.lvRecords.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.StudentName,
+            this.StudentID,
+            this.Course});
+            this.lvRecords.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvRecords.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lvRecords.FullRowSelect = true;
+            this.lvRecords.GridLines = true;
+            this.lvRecords.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.lvRecords.HideSelection = false;
+            this.lvRecords.Location = new System.Drawing.Point(532, 259);
+            this.lvRecords.Margin = new System.Windows.Forms.Padding(2);
+            this.lvRecords.MultiSelect = false;
+            this.lvRecords.Name = "lvRecords";
+            this.lvRecords.Size = new System.Drawing.Size(383, 342);
+            this.lvRecords.TabIndex = 0;
+            this.lvRecords.UseCompatibleStateImageBehavior = false;
+            this.lvRecords.View = System.Windows.Forms.View.Details;
+            // 
+            // StudentName
+            // 
+            this.StudentName.Text = "Student Name";
+            this.StudentName.Width = 150;
+            // 
+            // StudentID
+            // 
+            this.StudentID.Text = "Student ID";
+            this.StudentID.Width = 125;
+            // 
+            // Course
+            // 
+            this.Course.Text = "Course";
+            this.Course.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Course.Width = 104;
+            // 
+            // DataGV1
+            // 
+            this.DataGV1.AllowUserToAddRows = false;
+            this.DataGV1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DataGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGV1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.DataGV1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.DataGV1.Location = new System.Drawing.Point(13, 259);
+            this.DataGV1.Margin = new System.Windows.Forms.Padding(2);
+            this.DataGV1.MultiSelect = false;
+            this.DataGV1.Name = "DataGV1";
+            this.DataGV1.RowHeadersWidth = 62;
+            this.DataGV1.RowTemplate.Height = 28;
+            this.DataGV1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.DataGV1.Size = new System.Drawing.Size(499, 341);
+            this.DataGV1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Student Name";
+            this.Column1.MinimumWidth = 10;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 175;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Student ID";
+            this.Column2.MinimumWidth = 10;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 145;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Course";
+            this.Column3.MinimumWidth = 10;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 115;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft YaHei", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnAdd.Location = new System.Drawing.Point(623, 83);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(96, 45);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Tomato;
+            this.lblName.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Black;
+            this.lblName.Location = new System.Drawing.Point(254, 86);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(84, 19);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Username:";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lbl_ID
+            // 
+            this.lbl_ID.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_ID.AutoSize = true;
+            this.lbl_ID.BackColor = System.Drawing.Color.Red;
+            this.lbl_ID.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ID.ForeColor = System.Drawing.Color.Black;
+            this.lbl_ID.Location = new System.Drawing.Point(250, 120);
+            this.lbl_ID.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_ID.Name = "lbl_ID";
+            this.lbl_ID.Size = new System.Drawing.Size(88, 19);
+            this.lbl_ID.TabIndex = 4;
+            this.lbl_ID.Text = "Student ID:";
+            this.lbl_ID.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(331, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 36);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "REGISTAR";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.Color.Yellow;
+            this.btnRemove.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemove.Font = new System.Drawing.Font("Microsoft YaHei", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnRemove.Location = new System.Drawing.Point(623, 200);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(96, 45);
+            this.btnRemove.TabIndex = 10;
+            this.btnRemove.Text = "REMOVE";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Font = new System.Drawing.Font("Microsoft YaHei", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(337, 83);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(195, 27);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtID
+            // 
+            this.txtID.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtID.Font = new System.Drawing.Font("Microsoft YaHei", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(337, 117);
+            this.txtID.Margin = new System.Windows.Forms.Padding(2);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(195, 27);
+            this.txtID.TabIndex = 6;
+            // 
+            // lblCourse
+            // 
+            this.lblCourse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCourse.AutoSize = true;
+            this.lblCourse.BackColor = System.Drawing.Color.Red;
+            this.lblCourse.Font = new System.Drawing.Font("Microsoft YaHei", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCourse.ForeColor = System.Drawing.Color.Black;
+            this.lblCourse.Location = new System.Drawing.Point(277, 155);
+            this.lblCourse.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCourse.Name = "lblCourse";
+            this.lblCourse.Size = new System.Drawing.Size(61, 19);
+            this.lblCourse.TabIndex = 7;
+            this.lblCourse.Text = "Course:";
+            this.lblCourse.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txtCourse.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCourse.Font = new System.Drawing.Font("Microsoft YaHei", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCourse.Location = new System.Drawing.Point(337, 152);
+            this.txtCourse.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(195, 27);
+            this.txtCourse.TabIndex = 8;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft YaHei", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.Maroon;
+            this.btnClear.Location = new System.Drawing.Point(623, 142);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(96, 45);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // picHeader
+            // 
+            this.picHeader.Image = ((System.Drawing.Image)(resources.GetObject("picHeader.Image")));
+            this.picHeader.Location = new System.Drawing.Point(-3, -2);
+            this.picHeader.Name = "picHeader";
+            this.picHeader.Size = new System.Drawing.Size(934, 272);
+            this.picHeader.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picHeader.TabIndex = 13;
+            this.picHeader.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 613);
+            this.Controls.Add(this.txtCourse);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCourse);
+            this.Controls.Add(this.lbl_ID);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lvRecords);
+            this.Controls.Add(this.DataGV1);
+            this.Controls.Add(this.picHeader);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DataGV1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHeader)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvRecords;
+        private System.Windows.Forms.DataGridView DataGV1;
+        private System.Windows.Forms.ColumnHeader StudentName;
+        private System.Windows.Forms.ColumnHeader StudentID;
+        private System.Windows.Forms.ColumnHeader Course;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lbl_ID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblCourse;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.PictureBox picHeader;
+    }
+}
+
---
+++ ListView_and_DataGridView/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListView_and_DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+                string.IsNullOrWhiteSpace(txtID.Text) ||
+                string.IsNullOrWhiteSpace(txtCourse.Text))
+            {
+                MessageBox.Show("Please fill in all fields.", "Input Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            foreach (ListViewItem item in lvRecords.Items)
+            {
+                if (item.Text == txtID.Text)
+                {
+                    MessageBox.Show("A student with this ID already exists.", "Duplicate ID",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+
+            ListViewItem lvItem = new ListViewItem(txtID.Text);
+            lvItem.SubItems.Add(txtName.Text);
+            lvItem.SubItems.Add(txtCourse.Text);
+            lvRecords.Items.Add(lvItem);
+
+            DataGV1.Rows.Add(txtID.Text, txtName.Text, txtCourse.Text);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtID.Clear();
+            txtCourse.Clear();
+            txtName.Focus();
+            if (lvRecords.SelectedIndices.Count == 0 && DataGV1.SelectedRows.Count == 0)
+            { return; }
+            int index = -1;
+            if (lvRecords.SelectedIndices.Count > 0)
+            {
+                index = lvRecords.SelectedIndices[0];
+            }
+            else
+            {
+                index = DataGV1.SelectedRows[0].Index;
+            }
+
+            if (index >= 0)
+            {
+                lvRecords.Items.RemoveAt(index);
+                if (index < DataGV1.Rows.Count && !DataGV1.Rows[index].IsNewRow)
+                {
+                    DataGV1.Rows.RemoveAt(index);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtID.Clear();
+            txtCourse.Clear();
+            lvRecords.Items.Clear();
+            DataGV1.Rows.Clear();
+            txtName.Focus();
+        }
+    }
+}
